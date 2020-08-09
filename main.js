c9d513aaa1516b453befe335ab7efb2d34f7262e
Kitti Kovacs <bambula86@gmail.com>
2020-08-09 19:30:36 +0000

    AJAX test

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
-xhr.onreadystatechange=function(){
-    xhr.onreadystatechange = function() {
+
+xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         document.getElementById("data").innerHTML = this.responseText;
     }
