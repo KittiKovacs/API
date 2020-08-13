72c70f84eb7b251b654eb6b84b75e0d497e2e8bb
Kitti Kovacs <bambula86@gmail.com>
2020-08-13 13:19:43 +0000

    unpacking data

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-var xhr = new XMLHttpRequest();
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-xhr.onreadystatechange = function() {
+function getData(type, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", baseURL + type + "/");
+  xhr.send();
+
+  xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
+      cb(JSON.parse(this.responseText));
     }
-};
+  };
+}
+function writeToDocument(type) {
+    var el= document.getElementById("data");
+    el.innerHTML="";
+  getData(type, function (data) {
+    data = data.results;
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-
-xhr.send();
+    data.forEach(function (item) {
+      el.innerHTML += "<p>" + item.name+ "</p>";
+    });
+  });
+}
